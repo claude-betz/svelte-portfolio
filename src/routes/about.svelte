290a653e4d7cb1e85c56700b7de3e8ba413e728d
Claude Betz <claudebz@amazon.com>
2022-03-23 20:48:30 +0200

    Added some pages to site

---
+++ src/routes/about.svelte
@@ -0,0 +1,49 @@
+<!-- src/routes/about.svelte -->
+<svelte:head>
+    <title>About</title>
+</svelte:head>
+
+<div class="main_container">
+    <section>
+        <div class="inner-container">
+            <h2 class="about-title">I'm Claude Betz👋</h2>
+            <div class="about-subtitle">
+                <p>
+
+                </p>
+                <p>
+                    Just trying to figure it all out. Reach out at
+                    <span class="info-text">claude.betz47@gmail.com</span>
+                </p>
+            </div>
+        </div>
+    </section>
+</div>
+
+<style>
+    .main_container {
+        margin: 0 80px 0 65px;
+    }
+
+    .about-title {
+        font-size: 4.2rem;
+        font-weight: bold;
+        margin-top: 11rem;
+    }
+
+    .about-subtitle {
+        margin-top: -2rem;
+    }
+
+    .about-subtitle >p {
+        width: 60%;
+        font-weight: normal;
+        font-size: 1rem;
+        line-height: 2rem;
+    }
+
+    .info-text {
+        font-style: italic;
+        font-weight: bold;
+    }
+</style>
---
+++ src/routes/blog.svelte
@@ -0,0 +1,37 @@
+<!-- src/routes/blog.svelte -->
+<svelte:head>
+    <title>Blog</title>
+</svelte:head>
+
+<script context="module">
+    export const load = async ({ fetch }) => {
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const blogposts = await res.json();
+        return {
+            props: {
+                blogposts,
+            }
+        }
+    }
+</script>
+
+<script>
+    export let blogposts;
+</script>
+
+<div class="container">
+    <h1>My Articles</h1>
+    <div class="blogposts">
+        {#each blogposts as post}
+            <div class="post">
+                <h2>{post.title.substring(0, 20)}</h2>
+                <p>{post.body.substring(0, 80)}</p>
+                <p class="readmore">
+                    <a style=" color: rgb(10, 10, 139);" href={`/blog/${post.id}`}>
+                        Read More
+                    </a>
+                </p>
+            </div>
+        {/each}
+    </div>
+</div>
