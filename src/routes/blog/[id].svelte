fda5ab7d99db1b0ed884eaf8b577e86064f92156
Claude Betz <claudebz@amazon.com>
2022-03-23 21:44:18 +0200

    Fixed dynamic route generation

---
+++ src/routes/blog/[id].svelte
@@ -0,0 +1,24 @@
+<script context="module">
+    export const load = async ({ fetch, params }) => {
+        const { id } = params
+
+        const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+        const post = await res.json();
+
+        return {
+            props: {
+                post,
+            }
+        }
+    }
+</script>
+
+<script>
+    export let post;
+</script>
+
+<div class="container">
+    <h1>
+        ${post.body}
+    </h1>
+</div>
